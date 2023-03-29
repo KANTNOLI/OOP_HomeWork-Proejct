74e7cf8663f5fbe4d649fd684668cab294439efd
KANTNOLI <zahargysev02@gmail.com>
2023-03-29 21:38:26 +0300

    add class + public and private constructions!!!

--- OOP_HomeWork-Proejct/Animals.h
+++ OOP_HomeWork-Proejct/Animals.h
@@ -1,3 +1,51 @@
 #pragma once
 
 #include "main.h"
+
+
+class Zoo {
+private:
+	float voracity;//прожорливость 
+	float weight;  //вес
+	string type;   //хищник и тд
+	string name;   //лев и т.д
+
+
+public:
+	float get_voracity() {
+		return voracity;
+	}
+	void set_voracity(float v) {
+		if (v > 0) {
+			voracity = v;
+		}
+	}
+
+	
+	float get_weight() {
+		return weight;
+	}
+	void set_weight(float w) {
+		if (w > 0) {
+			weight = w;
+		}
+	}
+
+	string get_type() {
+		return type;
+	}
+	void set_type(string t) {
+		type = t;
+	}
+
+
+	string get_name() {
+		return name;
+	}
+	void set_name(string n) {
+		name = n;
+	}
+
+
+
+};
